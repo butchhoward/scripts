991fdebbeaa4558d36a37b89f5f2ba0b70f1d75c
Butch Howard <butchhoward@gmail.com>
2018-02-13 13:28:18 -0600

    Better filter on docker_rmi_matching

--- docker_tools.sh
+++ docker_tools.sh
@@ -10,8 +10,19 @@ function docker_rmi_dangling()
     docker rmi $(docker images -f "dangling=true" -q)
 }
 
+
+# uses the docker images --filter flag
+# if your image names have slashes (/) you MUST wildcard+escape them
+# REPOSITORY                                       TAG
+# drydock.workiva.net/workiva/cds                  0.0.68
+#
+# docker_rmi_matching "drydock*\/*\/*"
+#
+# use 
+# docker images --filter=reference="drydock*\/*\/*"
+# to preview the list of images that will be removed
 function docker_rmi_matching()
 {
     local MATCH_TARGET=${1:?"Give me something to match!"}
-    docker rmi $(docker images -q "${MATCH_TARGET}")
+    docker rmi $(docker images -qa --filter=reference="${MATCH_TARGET}")
 }
