8358707cc21d428220a5e3146c986d89e67e4166
Butch Howard <butchhoward@gmail.com>
2018-09-14 13:41:01 -0400

    shellcheck lint

--- scripts.sh
+++ scripts.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 #source this script to get the useful functions
 LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source ${LOCATION}/venv_tools.sh
-source ${LOCATION}/repo_tools.sh
-source ${LOCATION}/docker_tools.sh
+source "${LOCATION}"/venv_tools.sh
+source "${LOCATION}"/repo_tools.sh
+source "${LOCATION}"/docker_tools.sh
