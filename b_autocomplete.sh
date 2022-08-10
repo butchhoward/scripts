fda3d0c48f9cedfb114aeb05810909da07b43de9
Butch Howard <bhowar68@ford.com>
2022-08-10 14:41:55 -0400

    Update tab completion

--- b_autocomplete.sh
+++ b_autocomplete.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-for s in repo bdocker baz venv bimage; do
+for s in repo bdocker baz venv bimage bnpm; do
     complete -o bashdefault -o default -W "$($s | tr '\n' ' ')" "$s"
 done
